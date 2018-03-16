fde00c77215c60a2ff53c045846929399c322548
solofruad <nestorsuat>
2018-03-15 21:54:29 -0500

    Archivo automatizador de tareas
    En este archivo hemos agregado un automatizador para que ejecute browserify y watchify sin necesidad de hacerlo nosotros mismos.

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'), 
+    browserify = require('browserify'), 
+    watchify = require('watchify'), 
+    source = require('vinyl-source-stream');
+
+
+var entry = './app/index.js'; //Script de entrada 
+var args = watchify.args; 
+args.debug = true; //Genera el sourcemap para debuguear
+args.fullPaths = false; //Evita el uso de paths absolutos 
+var bundler = watchify(browserify(entry, args));
+
+
+function createBundle(){ 
+    console.log('Now building...'); 
+    return bundler.bundle() 
+        .pipe(source('main.js')) //Nombre del bundle final
+        .pipe(gulp.dest('./assets/js')); //Directorio de destino
+} 
+
+gulp.task('dev', createBundle); 
+bundler.on('update', createBundle); //A cada modificación generamos un nuevo bundle 
+
+//Proporcionamos algo de información al generar el bundle
+bundler.on('time', function(time){ 
+    console.log('Done at ' + (time/1000)); 
+});
+
