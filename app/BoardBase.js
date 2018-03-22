0cc34060fe6952d7a4d62385581c8333f7198ff9
daniing <dzapatame@unal.edu.co>
2018-03-21 23:50:15 -0500

    avance tablero de juego
    Se define la función BoardBase creando la configuracion para mostrar el tablero del juego

---
+++ app/BoardBase.js
@@ -0,0 +1,28 @@
+    function BoardBase()
+    {
+        pushPeg : function(holeId){
+            this.setPegExists(holeId, true);
+            return this;
+        };
+        pullPeg : function(holeId){
+            this.setPegExists(holeId, false);
+            return this;
+        };
+        movePeg : function(fromId, toId){
+            if(this.hasPeg(fromId) && this.hasEmptyHole(toId)){
+                var dir = this.getDirFromToDist2(fromId, toId);
+                if(dir != INVALID_DIR){
+                    var nextId = this.getAdjacent(fromId, dir);
+                    var nextNextId = this.getAdjacent(nextId, dir);
+                    if(this.hasPeg(nextId)){
+                        this.pushPeg(nextNextId);
+                        this.pullPeg(nextId);
+                        this.pullPeg(fromId);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        };
+        
+    }
