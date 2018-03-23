ee94e8dd36a6596913aafa7cd6279cf3c26eba08
solofruad <nestorsuat>
2018-03-22 23:36:38 -0500

    Agregando función eachHole para obtener información del agujero y ejecutar una función callback
    En la clase drawBoardToCanvas se hicieron las funciones de validación para ver si el hole esta disponible y habilitar su movimiento

--- app/BoardBase.js
+++ app/BoardBase.js
@@ -1,58 +1,66 @@
-    function BoardBase()
-    {
-        pushPeg : function(holeId){
-            this.setPegExists(holeId, true);
-            return this;
-        };
-        pullPeg : function(holeId){
-            this.setPegExists(holeId, false);
-            return this;
-        };
-        movePeg : function(fromId, toId){
-            if(this.hasPeg(fromId) && this.hasEmptyHole(toId)){
-                var dir = this.getDirFromToDist2(fromId, toId);
-                if(dir != INVALID_DIR){
-                    var nextId = this.getAdjacent(fromId, dir);
-                    var nextNextId = this.getAdjacent(nextId, dir);
-                    if(this.hasPeg(nextId)){
-                        this.pushPeg(nextNextId);
-                        this.pullPeg(nextId);
-                        this.pullPeg(fromId);
-                        return true;
-                    }
+module.exports = {
+    pushPeg : function(holeId){
+        this.setPegExists(holeId, true);
+        return this;
+    };
+    pullPeg : function(holeId){
+        this.setPegExists(holeId, false);
+        return this;
+    };
+    movePeg : function(fromId, toId){
+        if(this.hasPeg(fromId) && this.hasEmptyHole(toId)){
+            var dir = this.getDirFromToDist2(fromId, toId);
+            if(dir != INVALID_DIR){
+                var nextId = this.getAdjacent(fromId, dir);
+                var nextNextId = this.getAdjacent(nextId, dir);
+                if(this.hasPeg(nextId)){
+                    this.pushPeg(nextNextId);
+                    this.pullPeg(nextId);
+                    this.pullPeg(fromId);
+                    return true;
                 }
             }
-            return false;
-        };
-        undoMovePeg: function(fromId, toId){
-            if(this.hasEmptyHole(fromId) && this.hasPeg(toId)){
-                var dir = this.getDirFromToDist2(fromId, toId);
-                if(dir != INVALID_DIR){
-                    var nextId = this.getAdjacent(fromId, dir);
-                    var nextNextId = this.getAdjacent(nextId, dir);
-                    if(this.hasEmptyHole(nextId)){
-                        this.pullPeg(nextNextId);
-                        this.pushPeg(nextId);
-                        this.pushPeg(fromId);
-                        return true;
-                    }
+        }
+        return false;
+    };
+    undoMovePeg: function(fromId, toId){
+        if(this.hasEmptyHole(fromId) && this.hasPeg(toId)){
+            var dir = this.getDirFromToDist2(fromId, toId);
+            if(dir != INVALID_DIR){
+                var nextId = this.getAdjacent(fromId, dir);
+                var nextNextId = this.getAdjacent(nextId, dir);
+                if(this.hasEmptyHole(nextId)){
+                    this.pullPeg(nextNextId);
+                    this.pushPeg(nextId);
+                    this.pushPeg(fromId);
+                    return true;
                 }
             }
-            return false;
-        };
-        findHoleAtPosition : function(x, y, r, includingInvalidHoles){
-            if(!r){ r = 0.5;}
-            var count = this.getHoleCount();
-            for(var id = 0; id < count; ++id){
-                if(includingInvalidHoles || this.hasValidHole(id)){
-                    var dx = this.getHoleLayoutPositionX(id) - x;
-                    var dy = this.getHoleLayoutPositionY(id) - y;
-                    if(dx*dx+dy*dy < r*r)
-                        return id;
-                    
-                }
+        }
+        return false;
+    };
+    findHoleAtPosition : function(x, y, r, includingInvalidHoles){
+        if(!r){ r = 0.5;}
+        var count = this.getHoleCount();
+        for(var id = 0; id < count; ++id){
+            if(includingInvalidHoles || this.hasValidHole(id)){
+                var dx = this.getHoleLayoutPositionX(id) - x;
+                var dy = this.getHoleLayoutPositionY(id) - y;
+                if(dx*dx+dy*dy < r*r)
+                    return id;
+                
             }
-            return INVALID_HOLE_ID;
-        };
+        }
+        return INVALID_HOLE_ID;
+    };
 
-    }
+    eachHole : function(fun, includingInvalidHoles){
+        var holeCount = this.getHoleCount();
+        for(var id = 0; id < holeCount; ++id){
+            if(includingInvalidHoles || this.hasValidHole(id)){
+                fun(id);
+            }
+        }
+    };
+
+}   
--- app/drawBoardToCanvas.js
+++ app/drawBoardToCanvas.js
@@ -1,29 +1,60 @@
-drawBoardToCanvas(canvas, ctx, board, opt, draggingPeg, drawInvalidHoles)
-    {
-        ctx.clearRect(0,0,canvas.width, canvas.height);
-        var left = opt.paddingLeft;
-        var top = opt.paddingTop;
-        var holeSpanX = opt.holeSpanX;
-        var holeSpanY = opt.holeSpanY;
-        var holeRadius = opt.holeRadius;
-        var pegRadius = opt.pegRadius;
+module.exports = function(canvas, ctx, board, opt, draggingPeg, drawInvalidHoles) {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    var left = opt.paddingLeft;
+    var top = opt.paddingTop;
+    var holeSpanX = opt.holeSpanX;
+    var holeSpanY = opt.holeSpanY;
+    var holeRadius = opt.holeRadius;
+    var pegRadius = opt.pegRadius;
 
-        // Invalid Holes
-        if(drawInvalidHoles){
-            board.eachHole(function(holeId){
-                if(!board.hasValidHole(holeId)){
-                    var holeX = left + board.getHoleLayoutPositionX(holeId) * holeSpanX;
-                    var holeY = top  + board.getHoleLayoutPositionY(holeId) * holeSpanY;
-                    ctx.beginPath();
-                    ctx.moveTo(holeX-pegRadius, holeY);
-                    ctx.lineTo(holeX+pegRadius, holeY);
-                    ctx.moveTo(holeX, holeY-pegRadius);
-                    ctx.lineTo(holeX, holeY+pegRadius);
-                    ctx.strokeStyle = "black";
-                    ctx.lineWidth = 1;
-                    ctx.stroke();
-                }
-            }, true);
-        }
-
+    // Invalid Holes
+    if(drawInvalidHoles){
+        board.eachHole(function(holeId){
+            if(!board.hasValidHole(holeId)){
+                var holeX = left + board.getHoleLayoutPositionX(holeId) * holeSpanX;
+                var holeY = top  + board.getHoleLayoutPositionY(holeId) * holeSpanY;
+                ctx.beginPath();
+                ctx.moveTo(holeX-pegRadius, holeY);
+                ctx.lineTo(holeX+pegRadius, holeY);
+                ctx.moveTo(holeX, holeY-pegRadius);
+                ctx.lineTo(holeX, holeY+pegRadius);
+                ctx.strokeStyle = "black";
+                ctx.lineWidth = 1;
+                ctx.stroke();
+            }
+        }, true);
     }
+    // Hole
+    board.eachHole(function(holeId){
+        var holeX = left + board.getHoleLayoutPositionX(holeId) * holeSpanX;
+        var holeY = top  + board.getHoleLayoutPositionY(holeId) * holeSpanY;
+        ctx.beginPath();
+        ctx.arc(holeX, holeY, holeRadius, 0, Math.PI*2, false);
+        if(draggingPeg && holeId == draggingPeg.getDstHoleId() && board.canMoveFromTo(draggingPeg.getHoleId(), holeId)){
+            ctx.strokeStyle = "red";
+            ctx.lineWidth = 3;
+        }
+        else{
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 1;
+        }
+        ctx.stroke();
+    });
+
+    // Peg
+    board.eachHole(function(holeId){
+        if(board.hasPeg(holeId)){
+            var pegX = left + board.getHoleLayoutPositionX(holeId) * holeSpanX;
+            var pegY = top  + board.getHoleLayoutPositionY(holeId) * holeSpanY;
+            if(draggingPeg && holeId == draggingPeg.getHoleId()){
+                pegX += draggingPeg.getDeltaX();
+                pegY += draggingPeg.getDeltaY();
+            }
+            ctx.beginPath();
+            ctx.arc(pegX, pegY, pegRadius, 0, Math.PI*2, false);
+            ctx.fillStyle = "black";
+            ctx.fill();
+        }
+    });
+
+}
