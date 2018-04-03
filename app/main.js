2d0de4fa004813f90f571ecf69262bca0ae49f3b
German Camilo Rojas <gerojas@unal.edu.co>
2018-04-02 23:09:50 -0500

    Cambios sugeridos de interfaz

--- app/main.js
+++ app/main.js
@@ -5,5 +5,8 @@ window.INVALID_HOLE_ID = -1;
 window.INVALID_DIR = -1;   
 
 $(document).ready(function() {
-	$("#gameBoard").append(createGameBox({}))
+	var URL= window.location;
+    var d = document.getElementById("reset"); 
+    d.setAttribute("href", URL); 
+	$("#gameBoard").append(createGameBox({}));
 });
